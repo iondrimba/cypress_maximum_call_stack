8605b763a045a4094d2c5ccefa3ab9ebc50a877f
Ion D. Filho <ion.drimba@foxbit.com.br>
2021-04-23 10:27:07 -0300

    Add success case

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -1,22 +1,44 @@
 describe('Github page', () => {
-  it('takes snapshot', () => {
-    cy.visit('/');
-
-    cy.get('h1').should('contain', ' Where the world builds software ');
-
-    cy.percySnapshot();
-  });
-
-  describe('when clicked on upper login button', () => {
-    it('redirects to login', () => {
+  describe('working without percy snapshot', () => {
+    it('takes snapshot', () => {
       cy.visit('/');
 
-      cy.get('a[href="/login]')
-        .click();
+      cy.get('h1').should('contain', ' Where the world builds software ');
+    });
 
-      cy.url().should('eq', `${Cypress.config().baseUrl}login`);
+    describe('when clicked on upper login button', () => {
+      it('redirects to login', () => {
+        cy.visit('/');
+
+        cy.get('a[href="/login"]')
+          .click();
+
+        cy.url().should('eq', `${Cypress.config().baseUrl}login`);
+      });
+    });
+  })
+
+  describe('not working with percy snapshot', () => {
+    it('takes snapshot', () => {
+      cy.visit('/');
+
+      cy.get('h1').should('contain', ' Where the world builds software ');
 
       cy.percySnapshot();
     });
-  });
+
+    describe('when clicked on upper login button', () => {
+      it('redirects to login', () => {
+        cy.visit('/');
+
+        cy.get('a[href="/login"]')
+          .click();
+
+        cy.url().should('eq', `${Cypress.config().baseUrl}login`);
+
+        cy.percySnapshot();
+      });
+    });
+  })
+
 });
