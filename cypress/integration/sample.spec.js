709f106c9186a2ff60882b4ce9e985b9a599de0d
Ion Drimba Filho <iondrimba@users.noreply.github.com>
2021-04-30 12:10:43 -0300

    Update sample.spec.js

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -1,44 +1,8 @@
 describe('Github page', () => {
-  describe('working without percy snapshot', () => {
-    it('takes snapshot', () => {
-      cy.visit('/');
-
-      cy.get('h1').should('contain', ' Where the world builds software ');
-    });
-
-    describe('when clicked on upper login button', () => {
-      it('redirects to login', () => {
-        cy.visit('/');
-
-        cy.get('a[href="/login"]')
-          .click();
-
-        cy.url().should('eq', `${Cypress.config().baseUrl}login`);
-      });
-    });
-  })
-
   describe('not working with percy snapshot', () => {
     it('takes snapshot', () => {
       cy.visit('/');
-
-      cy.get('h1').should('contain', ' Where the world builds software ');
-
       cy.percySnapshot();
     });
-
-    describe('when clicked on upper login button', () => {
-      it('redirects to login', () => {
-        cy.visit('/');
-
-        cy.get('a[href="/login"]')
-          .click();
-
-        cy.url().should('eq', `${Cypress.config().baseUrl}login`);
-
-        cy.percySnapshot();
-      });
-    });
-  })
-
+  });
 });
